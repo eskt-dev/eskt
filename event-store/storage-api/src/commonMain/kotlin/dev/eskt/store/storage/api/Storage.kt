9e05ebe4945a82e0b595268ed6ca26b935287ea0
Bruno Medeiros <brunojcm@gmail.com>
2023-08-04 22:49:39 +0800

    test(storage): change interface to return envelopes (#2)

--- event-store/storage-api/src/commonMain/kotlin/dev/eskt/store/storage/api/Storage.kt
+++ event-store/storage-api/src/commonMain/kotlin/dev/eskt/store/storage/api/Storage.kt
@@ -1,5 +1,6 @@
 package dev.eskt.store.storage.api
 
+import dev.eskt.store.api.EventEnvelope
 import dev.eskt.store.api.EventMetadata
 import dev.eskt.store.api.StreamType
 
@@ -10,7 +11,7 @@ public interface Storage {
     @Throws(ExpectedVersionMismatch::class)
     public fun <I, E> add(streamType: StreamType<I, E>, streamId: I, version: Int, event: E, metadata: EventMetadata = emptyMap())
 
-    public fun <I, E> getEvent(streamType: StreamType<I, E>, position: Long): E
+    public fun <I, E> getEvent(streamType: StreamType<I, E>, position: Long): EventEnvelope<I, E>
 
-    public fun <I, E> getStreamEvent(streamType: StreamType<I, E>, streamId: I, version: Int): E
+    public fun <I, E> getStreamEvent(streamType: StreamType<I, E>, streamId: I, version: Int): EventEnvelope<I, E>
 }
