98d742aff2e06697e1ef970108d406b2a961f293
Bruno Medeiros <brunojcm@gmail.com>
2023-08-31 13:51:28 +0800

    chore(grade): allow supported platforms to be disabled

--- buildSrc/src/main/kotlin/platform.kt
+++ buildSrc/src/main/kotlin/platform.kt
@@ -1,23 +1,29 @@
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
-fun KotlinMultiplatformExtension.setupPlatforms() {
+fun KotlinMultiplatformExtension.setupPlatforms(jvm: Boolean = true, native: Boolean = true, node: Boolean = true) {
     // Enable the default target hierarchy
     @OptIn(ExperimentalKotlinGradlePluginApi::class)
     targetHierarchy.default()
 
-    jvm {
-        jvmToolchain(17)
-        withJava()
+    if (jvm) {
+        jvm {
+            jvmToolchain(17)
+            withJava()
+        }
     }
 
-    js(IR) {
-        nodejs()
+    if (node) {
+        js(IR) {
+            nodejs()
+        }
     }
 
-    linuxX64()
-//    TODO restore after https://github.com/square/okio/pull/1301
-//    linuxArm64()
-    macosX64()
-    macosArm64()
+    if (native) {
+        linuxX64()
+//        TODO restore after https://github.com/square/okio/pull/1301
+//        linuxArm64()
+        macosX64()
+        macosArm64()
+    }
 }
