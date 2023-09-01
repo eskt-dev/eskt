2518071a1f00dc5ff4139eaccf6a87ba738e207e
Bruno Medeiros <brunojcm@gmail.com>
2023-09-01 19:22:54 +0800

    chore(kotlin): upgrade to 1.9.10

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-    implementation(kotlin("serialization", version = "1.9.0"))
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+    implementation(kotlin("serialization", version = "1.9.10"))
     implementation("com.diffplug.spotless:spotless-plugin-gradle:6.19.0")
 }
