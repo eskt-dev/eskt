a595aad01167c5b2b246b625d2a48c0b37326b3a
Bruno Medeiros <brunojcm@gmail.com>
2023-11-17 10:59:59 +0800

    docs(example): upgrade to kotlin 1.9.20

--- app-examples/spring-webflux-r2dbc/build.gradle.kts
+++ app-examples/spring-webflux-r2dbc/build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.1.5"
 	id("io.spring.dependency-management") version "1.1.3"
-	kotlin("jvm") version "1.8.22"
-	kotlin("plugin.spring") version "1.8.22"
+	kotlin("jvm") version "1.9.20"
+	kotlin("plugin.spring") version "1.9.20"
 }
 
 group = "dev.eskt.example"
