2d3a799f3416b499d59cd7fffefd6ddbd0994f2f
Bruno Medeiros <brunojcm@gmail.com>
2023-11-20 11:13:36 +0800

    remove spotless

--- app-examples/car-management-hexagonal-arch/buildSrc/build.gradle.kts
+++ app-examples/car-management-hexagonal-arch/buildSrc/build.gradle.kts
@@ -13,5 +13,4 @@ kotlin {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
     implementation(kotlin("serialization", version = "1.9.20"))
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.19.0")
 }
