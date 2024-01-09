76e0383758fe6acdb8c01f3469e76006fbb3742d
Bruno Medeiros <brunojcm@gmail.com>
2024-01-09 16:52:12 +0800

    feat(kotlin): upgrade to 1.9.22 (#22)

--- app-examples/car-management-hexagonal-arch/spring-webflux-jpa-application/build.gradle.kts
+++ app-examples/car-management-hexagonal-arch/spring-webflux-jpa-application/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 	id("org.springframework.boot") version "3.1.5"
 	id("io.spring.dependency-management") version "1.1.3"
 	kotlin("jvm")
-	kotlin("plugin.spring") version "1.9.20"
-	kotlin("plugin.jpa") version "1.9.20"
+	kotlin("plugin.spring") version "1.9.22"
+	kotlin("plugin.jpa") version "1.9.22"
 }
 
 group = "dev.eskt.example"
