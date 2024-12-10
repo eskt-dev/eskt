a9fee2554e169d1a6e9f442356564080462ae9c6
Tom <80664757+tom-delalande@users.noreply.github.com>
2024-12-10 11:43:53 +0800

    feat(event-listener): set exponential backoff as default strategy (#52)
    Co-authored-by: Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>

--- hex-arch-adapters-common/src/commonMain/kotlin/dev/eskt/arch/hex/adapter/common/EventListenerExecutorConfig.kt
+++ hex-arch-adapters-common/src/commonMain/kotlin/dev/eskt/arch/hex/adapter/common/EventListenerExecutorConfig.kt
@@ -1,10 +1,8 @@
 package dev.eskt.arch.hex.adapter.common
 
-import kotlin.time.Duration.Companion.seconds
-
 public data class EventListenerExecutorConfig(
     public val threadPoolName: String = "evt-listener",
     public val threadPoolSize: Int = 4,
     public val batchSize: Int = 100,
-    public val errorBackoff: BackoffStrategy = BackoffStrategy.Constant(15.seconds),
+    public val errorBackoff: BackoffStrategy = BackoffStrategy.Exponential(),
 )
