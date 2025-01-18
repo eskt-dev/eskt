90d4128b193f5dd2890dfc97ffb294e27a89b4cc
Bruno Medeiros <brunojcm@gmail.com>
2025-01-18 20:01:40 +0800

    style: upgrade to ktlint 1.5.0

--- buildSrc/src/main/kotlin/standard-kotlin-multiplatform.gradle.kts
+++ buildSrc/src/main/kotlin/standard-kotlin-multiplatform.gradle.kts
@@ -13,12 +13,12 @@ kotlin {
 spotless {
     kotlin {
         target("**/src/**/*.kt")
-        ktlint("1.1.1")
+        ktlint("1.5.0")
             .setEditorConfigPath("${rootProject.projectDir}/.editorconfig")
     }
     kotlinGradle {
         target("**/build.gradle.kts", "**/settings.gradle.kts")
-        ktlint("1.1.1")
+        ktlint("1.5.0")
             .setEditorConfigPath("${rootProject.projectDir}/.editorconfig")
     }
 }
